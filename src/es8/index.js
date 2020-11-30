aed4c8b830b9bf40e297f55c816fe0bcac97d52e
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2020-11-30 16:47:31 -0500

    Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -24,3 +24,36 @@ const string = 'hello';
 console.log(string.padStart(7, 'hi'));
 console.log(string.padEnd(12, ' -----'));
 console.log('food'.padEnd(12, ' ------'));
+
+//
+const obj = {
+    name: 'Guadalupe',
+}
+
+//
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('Hello World'), 300)
+            : reject(new Error('Test Error'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+//
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+anotherFunction();
